f101d3307e3caaecb12d0882e9e16acc15a39122
Eliah Kagan <degeneracypressure@gmail.com>
2023-02-09 16:43:06 -0500

    Heavily refactor initialize.cmd for readability

--- initialize.cmd
+++ initialize.cmd
@@ -8,21 +8,7 @@ set "script_uuid=7d63c880-c89c-4bc4-b440-57e134bf08d9"
 set "script_name=%~nx0"
 set "temp_path=%TEMP%\%script_uuid%"
 
-call :delete %conf_path%
-
-:: If the host clones the repo Windows-style, have the container play along.
-call :push_in core.autocrlf
-
-:: Usually these are automatically set in the container, but not always.
-call :push_in user.name
-call :push_in user.email
-
-:: Test with a nonexistent git configuration variable. (FIXME: Remove this.)
-call :push_in user.avatar
-
-call :delete %temp_path%
-
-goto done
+goto begin
 
 :msg
     echo/%script_name%: %* >&2
@@ -38,8 +24,8 @@ goto done
     setlocal
     set "name=%1"
 
-    for /f "usebackq delims=" %%a in (`git config "%name%"`) do (
-        set "value=%%a"
+    for /f "usebackq delims=" %%G in (`git config "%name%"`) do (
+        set "value=%%G"
     )
 
     if %ERRORLEVEL% EQU 0 (
@@ -51,4 +37,22 @@ goto done
 
     exit /b
 
-:done
+:push_all_in
+    :: If the host clones the repo Windows-style, have the container play
+    :: along.
+    call :push_in core.autocrlf
+
+    :: Usually these are automatically set in the container, but not always.
+    call :push_in user.name
+    call :push_in user.email
+
+    :: Test with a nonexistent git configuration variable.
+    :: (FIXME: Remove this.)
+    call :push_in user.avatar
+
+    exit /b
+
+:begin
+    call :delete %conf_path%
+    call :push_all_in
+    call :delete %temp_path%
