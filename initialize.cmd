315ab4ca382c35e0b37c9481adb95f31a4f479a4
Eliah Kagan <degeneracypressure@gmail.com>
2023-02-10 05:32:27 -0500

    Call git config twice instead of using temp file
    This considerably simplifies and shortens the batch file.

--- initialize.cmd
+++ initialize.cmd
@@ -1,5 +1,3 @@
-@echo off
-
 :: Copyright (c) 2023 Eliah Kagan
 ::
 :: Permission to use, copy, modify, and/or distribute this software for any
@@ -13,14 +11,10 @@
 :: OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 :: CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+@echo off
 setlocal enableextensions
-
 set "conf_path=.inherited-configuration"
-set "script_uuid=7d63c880-c89c-4bc4-b440-57e134bf08d9"
-
 set "script_name=%~nx0"
-set "temp_path_prefix=%TEMP%\%script_uuid%-%RANDOM%"
-
 goto begin
 
 :msg
@@ -36,26 +30,18 @@ goto begin
 :push_in
     setlocal
     set "name=%1"
-    set "temp_path=%temp_path_prefix%-%name%.tmp"
 
-    call :delete_if_exist %temp_path%
+    git config -- "%name%" >NUL
 
-    git config -- "%name%" >"%temp_path%"
     if %ERRORLEVEL% NEQ 0 (
         call :msg skipping: %name%
-        del "%temp_path%"
         exit /b 0
     )
 
-    for /f "usebackq delims=" %%G in ("%temp_path%") do (
+    for /f "usebackq delims=" %%G in (`git config -- "%name%"`) do (
         set "value=%%G"
     )
-    if %ERRORLEVEL% NEQ 0 (
-        call :msg can't read temporary file: %temp_path%
-        exit 1
-    )
 
-    del "%temp_path%"
     call :msg carrying in: %name%=%value%
     echo/%name% %value% >>"%conf_path%"
     exit /b
